788301e7a7b3a55a809a58b2f8bc67e04b2949d7
Chase <Chasewb91@gmail.com>
2021-12-08 10:03:30 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ token=os.getenv('discord_token') #pulls the discord token from a .env file --
 
 # For the bot to launch with this set up, you must have a .env file with the following, excluding quotations "discord_token=DiscordBotTokenGoesHere" 
 
-intents = discord.Intents.default() # sets permissions
+intents = discord.Intents.default()
 intents.members = True
 
+client = commands.Bot(command_prefix='!', intents=intents)  #Makes the bot prefix. 
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!') #prints a confirmation in the terminal that it has connected to discord
+    await client.change_presence(activity=discord.Game('Scrabble'))  #sets the bots discord presence, such as playing "Yahtzee". 
+    
+client.run(TOKEN)
+
+    
